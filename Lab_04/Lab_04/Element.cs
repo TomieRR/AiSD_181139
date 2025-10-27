1cf421c443b0aeeab4f013b10e735fd4854b3803
TomieR <136272336+TomieRR@users.noreply.github.com>
2025-10-27 12:56:13 +0100

    Add files via upload

---
+++ Lab_04/Lab_04/Element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04
+{
+    internal class Element
+    {
+        public int wartosc;
+        public Element next;
+        public Element prev;
+
+        public Element(int wartosc)
+        {
+            this.wartosc = wartosc;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+}
---
+++ Lab_04/Lab_04/Lista.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_04;
+
+namespace Lab_04
+{
+    internal class Lista
+    {
+        public Element head;
+        public Element tail;
+        public int LiczbaElementow;
+
+        public void DodajPo(Element e, int liczba)
+        {
+            if (e == null)
+            {
+                return;
+            }
+             Element aktualny = e;
+
+            for(int i = 0; i < liczba; i++)
+            {
+                Element nowy = new Element(i + 1);
+
+                nowy.next = aktualny.next;
+
+                if(aktualny.next != null)
+                {
+                    aktualny.next.prev = nowy;
+                }
+                aktualny.next = nowy;
+                nowy.prev = aktualny;
+
+                if(nowy.next == null)
+                {
+                    tail = nowy;
+                }
+                LiczbaElementow++;
+
+                aktualny = nowy;
+            }
+
+            }
+        public void Dodajprzed(Element e, int liczba)
+        {
+            if (e == null)
+            {
+                return;
+            }
+            Element aktualny = e;
+
+
+        }
+        }
+
+    }
+
+
